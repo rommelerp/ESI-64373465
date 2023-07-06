5f46ac64cc87e4ddda14eb781780e0f08148e8e1
rommelerp <117048325+rommelerp@users.noreply.github.com>
2023-07-06 16:42:56 -0300

    Add files via upload

---
+++ API(Nueva)/autenticar.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Añadir</title>
+</head>
+
+<body>
+
+    <form action="autenticar.php" method="post">
+
+        <input type="hidden" name="accion" value="inicioSesion">
+        <b>ID:</b> <input type="number" name="id" required> <br />
+        <b>Nombre:</b> <input type="text" name="nombre" required> <br />
+        <b>Apellido:</b> <input type="text" name="apellido" required> <br />
+        <input type="submit" value="Entrar">
+    </form>
+
+    <?php
+    if ($_POST) {
+        $accion = $_POST['accion'];
+        $id = $_POST['id'];
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $url = 'http://localhost/Ej/API/api.php';
+
+        // Paso los datos a un array asociativo
+        $persona = array(
+            'accion' => $accion,
+            'id' => $id,
+            'nombre' => $nombre,
+            'apellido' => $apellido
+        );
+
+
+
+        // Datos a enviar en formato JSON
+    
+        $jsonData = json_encode($persona);
+
+        // Inicializar la solicitud cURL
+        $ch = curl_init($url);
+
+        // Establecer las opciones para enviar datos JSON
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($jsonData)
+            )
+        );
+
+        // Ejecutar la solicitud y obtener la respuesta
+        $response = curl_exec($ch);
+
+        // Cerrar la conexión cURL
+        curl_close($ch);
+    }
+    ?>
+</body>
+
+</html>
---
+++ API(Nueva)/usuarioIndex.php
@@ -0,0 +1,9 @@
+<?php
+echo "<a style='margin-left: 500px ;margin-right: 200px; font-size: 30px;' href='html/camioneros.html'>Camionarios</a>" .
+    "<a style='margin-right: 200px; font-size: 30px;' href='html/funcionarioAlmacen.html'>Funcionario de almacen</a>" .
+    "<a style='margin-right: 200px; font-size: 30px;' href='html/Admin.html'>Administrador</a>";
+
+
+
+
+?>
