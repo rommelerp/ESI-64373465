c4145a9e89492aa6632cc4de45c56abfe1419a24
rommelerp <117048325+rommelerp@users.noreply.github.com>
2023-07-06 16:44:56 -0300

    Delete usuarioIndex.php

--- usuarioIndex.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<a style='margin-left: 500px ;margin-right: 200px; font-size: 30px;' href='html/camioneros.html'>Camionarios</a>" .
-    "<a style='margin-right: 200px; font-size: 30px;' href='html/funcionarioAlmacen.html'>Funcionario de almacen</a>" .
-    "<a style='margin-right: 200px; font-size: 30px;' href='html/Admin.html'>Administrador</a>";
-
-
-
-
-?>
