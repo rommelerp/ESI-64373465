249b15858ff80a983819bf9a4bde7a8e65ee45a6
rommelerp <117048325+rommelerp@users.noreply.github.com>
2023-07-06 16:38:43 -0300

    Update api.php

--- api.php
+++ api.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($data === null) {
         // Error al decodificar el JSON
-        http_response_code(400); 
+        http_response_code(400);
         echo json_encode(array('error' => 'JSON inválido'));
 
 
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             //Si ya hay una persona con el mismo id, no se REGISTRA al usuario
             if ($resultado->num_rows > 0) {
                 echo "<script>alert('YA existe ese usuario');</script>";
-                $redireccion = '<script>window.location.href = "html/error1.html?exito=true";</script>';
+                $redireccion = '<script>window.location.href = "registrar.php?exito=true";</script>';
                 echo $redireccion;
                 //Sino, se REGISTRA al usuario  
             } else {
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 
-        //Si accion es igual a inicioSesion se autentica para ver si se AUTORIZA o no al usuario a acceder a la pagina
+            //Si accion es igual a inicioSesion se autentica para ver si se AUTORIZA o no al usuario a acceder a la pagina
         } else if ($accion == 'inicioSesion') {
             $sentencia = "SELECT * FROM personas WHERE id = '$id'";
             $resultado = $conexion->query($sentencia);
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 //Sino, se NO SE AUOTORIZA al usuario  
             } else {
                 echo "<script>alert('NO existe ese usuario');</script>";
-                $redireccion = '<script>window.location.href = "html/error.html?exito=true";</script>';
+                $redireccion = '<script>window.location.href = "autenticar.php?exito=true";</script>';
                 echo $redireccion;
             }
         }
