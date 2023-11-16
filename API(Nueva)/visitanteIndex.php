96f79b8bf3c34a604b2a14dd3911eb8ffb32abdb
rommelerp <117048325+rommelerp@users.noreply.github.com>
2023-11-15 21:15:09 -0300

    Delete API(Nueva) directory

--- API(Nueva)/visitanteIndex.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1 style='font-size: 30px; margin-left: 750px; color: rgb(68, 65, 65)'>BIENVENIDO</h1>";
-echo "<button style='margin-top: 300px; margin-left: 750px; font-size: 20px;' ><a href='autenticar.php'>Iniciar sesión</a></button>" .
-"<button style='margin-left: 10px; font-size: 20px;' ><a href='registrar.php'>Registrarse</a></button>";
-?>
